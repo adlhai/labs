eaf06d751e2a7f0b4d6cd494a01025fe6b0c2129
Lilya2828 <123166985+Lilya2828@users.noreply.github.com>
2024-03-30 03:02:06 +0500

    main

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LabbTest2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LabbTest2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64e51347-1ca8-44e7-a14a-91ae33363caa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using labss;
+
+namespace LabbTest2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestGenerateRandomArray()
+        {
+            RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+            int [] array = randomNumberGenerator.GenerateRandomArray();
+
+            Assert.IsNotNull(array);
+            Assert.AreEqual(array.Length, 8);
+
+            foreach (int num in array)
+            {
+                Assert.IsTrue(num >= 1 && num <= 700);
+            }
+        }
+    }
+}
