49366495a940fad9e49b57ec08b36c24fc2bdccc
adlhai <123166886+adlhai@users.noreply.github.com>
2024-03-30 01:35:48 +0500

    first commit

---
+++ RandomNumberGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labss
+{
+    public class RandomNumberGenerator
+    {
+        private Random random = new Random();
+        public int GenerateRandomNumber()
+        {
+            return random.Next(1, 701);
+        }
+    }
+}
